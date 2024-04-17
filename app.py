5e714c4bb6ef95696a99f6a37eab58e03b35da24
jzagaeski <164560992+jzagaeski@users.noreply.github.com>
2024-04-17 18:19:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-cars_df = pd.read_csv('/vehicles_us.csv')
+cars_df = pd.read_csv('vehicles_us.csv')
 cars_df['make'] = cars_df['model'].apply(lambda x: x.split()[0])
 st.header('Data Viewer')
 st.dataframe(cars_df)
 st.header('Vehicle condition relative to price by type')
 car_scat = px.scatter(cars_df, x='condition', y='price', color='type')
 st.write(car_scat)
-st.header('Compare vehicle type by condition')
+st.header('Compare vehicle condition by type')
 type_list = sorted(cars_df['type'].unique())
 type_1 = st.selectbox(label='Select type 1',
                       options=type_list,
