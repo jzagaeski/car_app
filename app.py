35694549fa5b372b509c1e600c172a08c6fa732e
jzagaeski <164560992+jzagaeski@users.noreply.github.com>
2024-04-20 19:42:05 -0400

    Update app.py checkbox

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ st.header('Vehicle condition relative to price by type')
 car_scat = px.scatter(cars_df, x='condition', y='price', color='type')
 st.write(car_scat)
 st.header('Compare vehicle condition by type')
+car_hist = px.histogram(cars_df, x='condition', nbins=35, color='type', histshow=histshow, barmode='overlay')
 show = st.checkbox(label='show histogram', value=True)
 if show:
     histshow = st.write(car_hist)
 else:
-    histshow= None
-car_hist = px.histogram(cars_df, x='condition', nbins=35, color='type', histshow=histshow, barmode='overlay')
+    histshow = None
